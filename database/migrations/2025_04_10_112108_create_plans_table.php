ca3d38e54c6af26f52a92c05405ae965f8926fa8
Programming party <kuroshkhamushi@gmail.com>
2025-04-10 05:14:40 -0700

    Update 2025_04_10_112108_create_plans_table.php

--- database/migrations/2025_04_10_112108_create_plans_table.php
+++ database/migrations/2025_04_10_112108_create_plans_table.php
@@ -8,7 +8,8 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     */
+    */
+
     public function up(): void
     {
         Schema::create('plans', function (Blueprint $table) {
@@ -23,7 +24,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     */
+    */
+
     public function down(): void
     {
         Schema::dropIfExists('plans');
