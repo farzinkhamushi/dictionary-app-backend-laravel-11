7feeba7426759aac6aa68cb1473c1bdb3b916830
Programming party <kuroshkhamushi@gmail.com>
2025-04-11 03:49:54 -0700

    Update AdminController.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Definition;
+use App\Models\Word;
 
 class AdminController extends Controller
 {
     //
+    public function index()
+    {
+        $definitions = Definition::all();
+        $words = Word::all();
+        $synonyms = Synonym::all();
+        $plans = Plan::all();
+        $subscriptions = Subscription::all();
+    }
 }
