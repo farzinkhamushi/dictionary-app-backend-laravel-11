3f7e64a206957f487f83fe3c6cd9d51c0317a76f
Programming party <kuroshkhamushi@gmail.com>
2025-04-11 02:45:04 -0700

    Update Synonym.php

--- app/Models/Synonym.php
+++ app/Models/Synonym.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Synonym extends Model
 {
-    //
+    protected $fillable = [
+        'similars' , 'word_id'
+    ];
+
+    public function word()
+    {
+        return $this->belongsTo(Word::class);
+    }
 }
