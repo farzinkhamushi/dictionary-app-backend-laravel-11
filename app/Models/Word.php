8648fe341b6095d9452cf6e7a5ee94c9ebba48d0
Programming party <kuroshkhamushi@gmail.com>
2025-04-11 02:54:02 -0700

    Udating User and Word Models

--- app/Models/Word.php
+++ app/Models/Word.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Word extends Model
 {
-    //
+    protected $fillable = [
+        'slug' , 'name'
+    ];
+
+    public function definitions()
+    {
+        return $this->hasMany(Definition::class);
+    }
+
+    public function synonyms()
+    {
+        return $this->hasMany(Synonym::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
