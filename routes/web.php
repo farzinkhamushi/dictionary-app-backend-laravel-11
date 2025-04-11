d69ef33a899a44a60db8f03563e086e50416bfba
Programming party <kuroshkhamushi@gmail.com>
2025-04-11 06:14:51 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', [AdminController::class,'login'])->name('admin.login');
 Route::post('/admin/auth', [AdminController::class,'auth'])->name('admin.auth');
 
 //->middleware('admin')
-Route::prefix('admin')->group(function(){
+Route::prefix('admin')->middleware('admin')->group(function(){
     Route::get('dashboard', [AdminController::class,'index'])->name('admin.index');
     Route::post('logout', [AdminController::class,'logout'])->name('admin.logout');
 });
