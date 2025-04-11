c82d13ca33da908a4f98337866d4af52d24a9ab6
Programming party <kuroshkhamushi@gmail.com>
2025-04-11 03:27:11 -0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,9 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
         */
+        $this->call([
+            AdminSeeder::class
+        ]);
 
-        
     }
 }
