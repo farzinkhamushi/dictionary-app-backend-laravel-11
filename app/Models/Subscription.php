de5ba7f6acfd755303ee0ea2b7c7aefe5f55dd3b
Programming party <kuroshkhamushi@gmail.com>
2025-04-11 02:19:48 -0700

    Update Subscription.php

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
-    //
+    protected $fillable = [
+        'plan_id' , 'user_id' , 'current_period_end' , 'current_period_start' , 'stripe_plan_id' ,
+        'stripe_status' , 'stripe_subscription_id'
+    ];
+    public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
 }
