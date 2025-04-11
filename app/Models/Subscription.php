7060687ef483b7dda6d90f6dae622661dec7f0ba
Programming party <kuroshkhamushi@gmail.com>
2025-04-11 02:31:19 -0700

    Update Subscription.php

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -10,8 +10,23 @@ class Subscription extends Model
         'plan_id' , 'user_id' , 'current_period_end' , 'current_period_start' , 'stripe_plan_id' ,
         'stripe_status' , 'stripe_subscription_id'
     ];
+
+    protected function casts():array
+    {
+        return [
+            'current_period_start' => 'datetime' , 
+            'current_period_end' => 'datetime' ,
+        ];
+    }
+    
     public function plan()
     {
         return $this->belongsTo(Plan::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
