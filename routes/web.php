b177ca874eac133fc96e2d9211b7796cde6c4bbf
Programming party <kuroshkhamushi@gmail.com>
2025-04-11 05:56:37 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [AdminController::class,'login'])->name('admin.login');
 Route::post('/admin/auth', [AdminController::class,'auth'])->name('admin.auth');
 
-Route::prefix('admin')->middleware('admin')->group(function(){
+//->middleware('admin')
+Route::prefix('admin')->group(function(){
     Route::get('dashboard', [AdminController::class,'index'])->name('admin.index');
     Route::post('logout', [AdminController::class,'logout'])->name('admin.logout');
 });
