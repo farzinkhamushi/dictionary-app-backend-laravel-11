138e949e2d802da4ee2764f409ef287468fc3b4a
Programming party <kuroshkhamushi@gmail.com>
2025-04-12 05:24:47 -0700

    Create index.blade.php

---
+++ resources/views/admin/words/index.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.layouts.app')
+
+@section('title')
+    Words
+@endsection
+
+@section('content')
+    <div class="row my-5">
+        <div class="col-md-4">
+            @include('admin.layouts.sidebar')
+        </div>
+        <div class="col-md-8">
+            <div class="container">
+
+            </div>
+        </div>
+    </div>
+@endsection
