c8b923aa115d3d7bb6df4c392e800d4e8b01eb33
Programming party <kuroshkhamushi@gmail.com>
2025-04-12 07:38:39 -0700

    Create create.blade.php

---
+++ resources/views/admin/words/create.blade.php
@@ -0,0 +1,29 @@
+@extends('admin.layouts.app')
+
+@section('title')
+    Add New Word
+@endsection
+
+@section('content')
+    <div class="row my-5">
+        <div class="col-md-3">
+            @include('admin.layouts.sidebar')
+        </div>
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header bg-white">
+                    <h3 class="mt-2">
+                        Add new word
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6 mx-auto">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
