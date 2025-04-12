dedff460899d979d7fd6bb429dc2e9ea7a2f5b7c
Programming party <kuroshkhamushi@gmail.com>
2025-04-12 07:38:30 -0700

    Update index.blade.php

--- resources/views/admin/words/index.blade.php
+++ resources/views/admin/words/index.blade.php
@@ -19,6 +19,51 @@
                         <i class="fas fa-plus"></i>
                     </a>
                 </div>
+                <div class="card-body">
+                    <div
+                        class="table-responsive"
+                    >
+                        <table
+                            class="table table-primary"
+                        >
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Slug</th>
+                                    <th>
+
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($words as $key => $word)                                    
+                                    <tr>
+                                        <td scope="row">{{ $key += 1}}</td>
+                                        <td>{{ $word->name }}</td>
+                                        <td>{{ $word->slug }}</td>
+                                        <td>
+                                            <a href="{{route('admin.words.edit', $word->slug)}}"
+                                                class="btn btn-sm btn-warning">
+                                                <i class="fas fa-edit"></i>
+                                            </a>
+                                            <a href="#"
+                                                onclick="deleteItem({{ $word->id }})"
+                                                class="btn btn-sm btn-danger">
+                                                <i class="fas fa-trash"></i>
+                                            </a>
+                                            <form id="{{ $word->id }}" action="{{route('admin.words.destroy', $word->slug)}}" method="post">
+                                                @csrf
+                                                @method("DELETE")
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     </div>
