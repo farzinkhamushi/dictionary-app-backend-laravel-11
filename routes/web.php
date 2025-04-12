4955222eb006d8d557946753ac33da0dd8455eb2
Programming party <kuroshkhamushi@gmail.com>
2025-04-12 05:36:53 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\WordController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AdminController::class,'login'])->name('admin.login');
