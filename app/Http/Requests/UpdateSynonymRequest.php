6e7125e961717dc9251ab11c50fead72b58f8013
Programming party <kuroshkhamushi@gmail.com>
2025-04-20 03:45:23 -0700

    Update UpdateSynonymRequest.php

--- app/Http/Requests/UpdateSynonymRequest.php
+++ app/Http/Requests/UpdateSynonymRequest.php
@@ -11,7 +11,7 @@ class UpdateSynonymRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class UpdateSynonymRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'similars' => 'required|max:255',
+            'word_id' => 'required'
         ];
     }
 }
