f6999579cef0506925803a221cbf97d3c8a3012f
Programming party <kuroshkhamushi@gmail.com>
2025-04-20 03:45:18 -0700

    Update AddSynonymRequest.php

--- app/Http/Requests/AddSynonymRequest.php
+++ app/Http/Requests/AddSynonymRequest.php
@@ -11,7 +11,7 @@ class AddSynonymRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class AddSynonymRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'similars' => 'required|max:255',
+            'word_id' => 'required'
         ];
     }
 }
